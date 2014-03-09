d840a4d6bc0bfaff4fb0ed9b79ce116d25c6f7bc
Smitha Milli <smitha.milli@gmail.com>
2014-03-08 20:35:07 -0800

    Move template

--- stridecenter/settings.py
+++ stridecenter/settings.py
@@ -78,14 +78,29 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'sitestatic')
 
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+#STATIC_URL = 'http://static.eventable.co/'
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'sitetemplates'),
+    os.path.join(BASE_DIR, 'templates'),
 )
